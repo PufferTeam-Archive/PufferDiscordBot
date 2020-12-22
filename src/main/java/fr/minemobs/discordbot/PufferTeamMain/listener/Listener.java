fd46ee2c9848e28ad6860411a9decae73189afa8
ProjectS <issahakim94@gmail.com>
2020-12-22 18:47:18 +0100

    Join message can be configurable in the json Added a new Command
    *refreshJoinMessage

--- src/main/java/fr/minemobs/discordbot/PufferTeamMain/listener/Listener.java
+++ src/main/java/fr/minemobs/discordbot/PufferTeamMain/listener/Listener.java
@@ -1,30 +1,43 @@
 package fr.minemobs.discordbot.PufferTeamMain.listener;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import fr.minemobs.discordbot.PufferTeamMain.Infos;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.DisconnectEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Listener extends ListenerAdapter {
+
+    ArrayList<String> joinMsg = new ArrayList<>();
 
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         super.onReady(event);
         System.out.printf("%#s est pret \n", event.getJDA().getSelfUser());
-    }
-
-    ArrayList<String> joinMsg = new ArrayList<>();
-
-    public Listener(){
-        joinMsg.add("%s joined the game");
+        try {
+            joinMsg = readJsonForJoinMessages();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(!joinMsg.contains("%s joined the game")){
+            joinMsg.add("%s joined the game");
+        }
     }
 
     @Override
@@ -60,6 +73,7 @@ public class Listener extends ListenerAdapter {
                     event.getChannel().sendMessage("Ok le message à été ajouté").queue();
                     event.getMessage().addReaction("✔").queue();
                     joinMsg.add(formattedMessage);
+                    createJsonForJoinMessages(joinMsg);
                 }else{
                     event.getMessage().reply("Le message ne contient pas l'argument **%s**").queue();
                     event.getMessage().addReaction("❌").queue();
@@ -67,10 +81,64 @@ public class Listener extends ListenerAdapter {
             }else if(event.getMessage().getContentRaw().equalsIgnoreCase(Infos.PREFIX.getS() + "ping")){
                 event.getChannel().sendMessage("Discord : " + event.getJDA().getGatewayPing() + " ms").queue();
                 event.getChannel().sendMessage("Api : " + event.getJDA().getRestPing().complete() + " ms").queue();
+            }else if(args[0].equalsIgnoreCase(Infos.PREFIX.getS() + "refreshJoinMessage")){
+                if(event.getGuild().getMember(event.getAuthor()).hasPermission(Permission.ADMINISTRATOR)){
+                    joinMsg.clear();
+                    try {
+                        joinMsg.addAll(readJsonForJoinMessages());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    event.getMessage().reply("Le json à bien été refresh").queue();
+                    event.getMessage().addReaction("✔").queue();
+                }else{
+                    event.getMessage().reply("You don't have the permission to use this command").queue();
+                    event.getMessage().addReaction("❌");
+                }
             }
         }else{
             event.getMessage().reply("Je n'accepte pas les DMs \n" +
                     "I don't accept DMs").queue();
+        }
+    }
+
+    private ArrayList readJsonForJoinMessages() throws IOException {
+
+        File file = new File("test.json");
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        Gson gson = new Gson();
+
+        Reader reader = Files.newBufferedReader(file.toPath());
+
+        ArrayList<String> msg = new ArrayList<>();
+
+        String[] welcomeMessage = gson.fromJson(reader, String[].class);
+        for (String s : welcomeMessage) {
+            msg.add(s);
+        }
+        return msg;
+    }
+
+    private void createJsonForJoinMessages(ArrayList<String> message) {
+        try{
+            File file = new File("test.json");
+
+            ArrayList<String> stringArray = new ArrayList<>();
+
+            for (String s : joinMsg) {
+                if(!readJsonForJoinMessages().contains(s)){
+                    stringArray.add(s);
+                }
+            }
+
+            Writer writer = Files.newBufferedWriter(Paths.get(file.toURI()));
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(stringArray, writer);
+            writer.close();
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
